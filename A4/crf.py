f89f384e4d6f478f9993532b1662006f8dacf3c7
Apoorv Sharma <sharmava@uw.edu>
2021-02-15 11:11:45 -0800

    Initial Viterbi Algo

--- A4/crf.py
+++ A4/crf.py
@@ -5,8 +5,13 @@ Description:
 '''
 
 import os
-from pprint import pprint as pp
+import sys
 from collections import defaultdict as dd
+from pprint import pprint as pp
+
+from tqdm import tqdm
+import numpy as np
+np.set_printoptions(threshold=sys.maxsize)
 
 START = '<start>'
 MASK = '<mask>'
@@ -31,11 +36,28 @@ class BigramModel:
             prob = float(prob.strip())
 
             self.blm[w1][w2] = prob
+    
+        self._create_labels_dict()
+
+    def _create_labels_dict(self):
+        labels = list(self.blm.keys())
+        labels.sort()
+        labels.append(labels.pop(labels.index(EOS))) # add EOS to end
+        labels = [labels.pop(labels.index(START))] + labels # add START to beginning
+
+        self.label_to_idx = {k: v for v, k in enumerate(labels)}
+        self.idx_to_label = {v: k for v, k in enumerate(labels)}
+
+    def get_labels_to_index(self):
+        return self.label_to_idx
+    
+    def get_index_to_labels(self):
+        return self.idx_to_label
             
     def get_bigram_prob(self, w1, w2) -> float:
         if w1 in self.blm.keys() and w2 in self.blm[w1].keys():
             return self.blm[w1][w2] # p(w2 | w1) = prob
-        return -1
+        return 0
     
     def get_max_w2(self, w1) -> str:
         return max(self.blm[w1], key=self.blm[w1].get)
@@ -72,6 +94,49 @@ class Viterbi:
         
         return guessed_complete_sentences
     
+    def get_transition_probability(self, labels, sentence_chars):
+        R, C = len(labels), len(sentence_chars)
+        tp = np.ones((R, C))
+        tp = tp / np.sum(tp, axis=0)
+
+        return tp
+    
+    def get_emission_probability(self, blm, labels, sentence_chars):
+        R, C = len(labels), len(sentence_chars)
+        ep = np.zeros((R, C))
+
+        for j in range(1, len(sentence_chars)):
+            w1, w2 = sentence_chars[j-1], sentence_chars[j]
+            
+            # if w1 == MASK:
+            #     continue
+            
+            for label, i in labels.items():
+                ep[i][j] = blm.get_bigram_prob(label, w1)
+
+                if label == w2: #??: DO WE NEED THIS ?
+                    ep[i][j] = 1
+            
+            w2 = blm.get_index_to_labels()[np.argmax(ep, axis=0)[j]]
+            sentence_chars[j-1], sentence_chars[j] = w1, w2
+
+        return ep
+
+    def viterbi_algorithm(self, blm: BigramModel):
+        labels = blm.get_labels_to_index()
+
+        guessed_complete_sentences = []
+        for sentence in tqdm(self.sentences, desc="Running Viterbi Algorithm"):
+            guessed_sentence = sentence[:]
+            N = len(guessed_sentence)
+
+            tp = self.get_transition_probability(labels, guessed_sentence)
+            ep = self.get_emission_probability(blm, labels, guessed_sentence)
+
+            guessed_complete_sentences.append(guessed_sentence)
+        
+        return guessed_complete_sentences
+    
     def write_sentences_to_file(self, sentence_list, file_path):
         sentence_list_strings = []
         
@@ -89,7 +154,9 @@ if __name__ == "__main__":
     print("NLP - A4")
 
     blm = BigramModel('./lm.txt')
+
     v = Viterbi('./15pctmasked.txt')
 
-    complete_sentences = v.bi_gram_prob(blm)
+    # complete_sentences = v.bi_gram_prob(blm)
+    complete_sentences = v.viterbi_algorithm(blm)
     v.write_sentences_to_file(complete_sentences, './unmasked.txt')

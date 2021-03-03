1938cdd63c55d583bb34b0e5a1217ff64e906000
Apoorv Sharma <sharmava@uw.edu>
2021-03-03 13:11:13 -0800

    Added encode word function

--- A5/byte_pair_encoding.py
+++ A5/byte_pair_encoding.py
@@ -69,17 +69,18 @@ class BytePairEncoding:
         return pairs
 
 
-    def _merge_vocab(self, char_pair_to_merge):
+    def _merge_vocab(self, char_pair_to_merge, vocab):
         
         modified_vocab = {}
 
         bigram = re.escape(' '.join(char_pair_to_merge))
         p = re.compile(r'(?<!\S)' + bigram + r'(?!\S)')
 
-        for word in self.vocab:
+        for word in vocab:
             # replace most frequent pair in all vocabulary
             w_out = p.sub(''.join(char_pair_to_merge), word)
-            modified_vocab[w_out] = self.vocab[word]
+            # print(''.join(char_pair_to_merge), w_out)
+            modified_vocab[w_out] = vocab[word]
         
         return modified_vocab
     
@@ -106,16 +107,19 @@ class BytePairEncoding:
         return len(self.types_vocab), corpus_length
 
 
-    def _save_information(self, descriptive_name, x, y, num_iters):
+    def _save_information(self, descriptive_name, x, y, num_iters, merge_pairs_list):
         xx, yy = self.get_data_lengths()
 
         with open(f"{descriptive_name}.out", 'w') as f:
-            f.write(f'Number of Iterations Completed: {num_iters}\n')
+            f.write(f'Number of Iterations Completed: {num_iters}\n\n')
             f.write(f'Length of Types Vocab: {xx}\n')
-            f.write(f'Frequency Sum of Types Vocab: {sum(self.char_pairs.values())}\n')
+            f.write(f'Frequency Sum of Types Vocab: {sum(self.char_pairs.values())}\n\n')
             f.write(f'Length of Training Vocab: {len(self.vocab)}\n')
             f.write(f'Frequency of Training Vocab: {sum(self.vocab.values())}\n')
-            f.write(f'Length of Training Data Under Types: {yy}\n')
+            f.write(f'Length of Training Data Under Types: {yy}\n\n')
+
+        with open(f"{descriptive_name}_merge_rules.out", 'w') as f:
+            f.writelines([f"{item[0]}||{item[1]}\n" for item in merge_pairs_list])
         
         def remap_keys(mapping):
             return [{'key':k, 'value': v} for k, v in mapping.items()]
@@ -129,12 +133,30 @@ class BytePairEncoding:
         self._save_scatter_plot(descriptive_name, x, y)
 
 
+    def encode_word(self, ordered_merge_pairs_path, word):
+        ordered_merge_pairs = []
+        with open(ordered_merge_pairs_path, 'r') as f:
+            ordered_merge_pairs = f.readlines()
+        
+        ordered_merge_pairs = self._clean_sentences(ordered_merge_pairs)
+
+        word_l = self._clean_sentences([word])
+        vocab, word_l = self._add_word_space_symbols(word_l)
+
+        for pair in ordered_merge_pairs:
+            pair = pair.split("||")
+            vocab = self._merge_vocab(pair, vocab)
+        
+        return list(vocab.keys())[0].split(' ')
+
+
     def fit(self):
 
         with open('vocab_initial.json', 'w') as f:
             json.dump(self.vocab, f, indent=4, sort_keys=True)
 
         x, y = [], []
+        merge_pairs_list = []
 
         i = 0
         is_frequency_one = False
@@ -155,15 +177,16 @@ class BytePairEncoding:
             
             top_char_pair = self._get_most_frequent_bigram(self.char_pairs)
             top_pair_frequency = self.char_pairs[top_char_pair]
+            merge_pairs_list.append(top_char_pair)
 
             # Track output
             if i % 1000: print(f"Current top pair frequency: {top_pair_frequency}")
 
             if top_pair_frequency == 1 and not is_frequency_one:
-                self._save_information('frequency_one', x, y, i)
+                self._save_information('frequency_one', x, y, i, merge_pairs_list)
                 is_frequency_one = True
 
-            self.vocab = self._merge_vocab(top_char_pair)
+            self.vocab = self._merge_vocab(top_char_pair, self.vocab)
             
             # print(f"Bigram to Merge: {top_char_pair}")
             # pp(self.char_pairs)
@@ -172,7 +195,7 @@ class BytePairEncoding:
             # print(self.types_vocab)
             # print()
 
-        self._save_information('frequency_zero', x, y, i)
+        self._save_information('frequency_zero', x, y, i, merge_pairs_list)
 
 
     def _get_most_frequent_bigram(self, char_pairs):
@@ -189,4 +212,7 @@ if __name__ == "__main__":
 
     bpe = BytePairEncoding("A5-data.txt")
     bpe.fit()
+    
+    with open(f"frequency_one.out", 'a') as f:
+        f.writelines([f"{item} " for item in bpe.encode_word('frequency_one_merge_rules.out', 'Acnestis')])
 
